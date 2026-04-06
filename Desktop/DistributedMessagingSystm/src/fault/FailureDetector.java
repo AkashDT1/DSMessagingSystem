818736d86a360394cd4249620b40a7640139d593
AkashDT1 <it24101853@my.sliit.lk>
2026-04-06 19:41:17 +0530

    detect node down

--- Desktop/DistributedMessagingSystm/src/fault/FailureDetector.java
+++ Desktop/DistributedMessagingSystm/src/fault/FailureDetector.java
@@ -1,17 +1,18 @@
 package fault;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class FailureDetector {
     
-    // basic ping logic started
-    public boolean checkPing(String host, int port) {
-        try {
-            Socket s = new Socket(host, port);
-            s.close();
+    // adding timeout to check if node down
+    public boolean checkNode(String host, int port) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(host, port), 2000);
             return true;
-        } catch (Exception e) {
+        } catch (IOException e) {
+            // failed so node is down
             return false;
         }
     }
